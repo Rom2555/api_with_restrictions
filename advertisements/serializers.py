6d8484deae9feca535558c780ab04a1b895582b0
Roman E <rk2555@yandex.ru>
2026-03-04 12:54:53 +0300

    добавлена проверка что у юзера не > 10 сообщений с OPEN

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -23,7 +23,7 @@ class AdvertisementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Advertisement
         fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at', )
+                  'status', 'created_at',)
 
     def create(self, validated_data):
         """Метод для создания"""
@@ -38,8 +38,32 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         return super().create(validated_data)
 
     def validate(self, data):
-        """Метод для валидации. Вызывается при создании и обновлении."""
+        """Метод для валидации. Вызывается при создании и обновлении"""
 
         # TODO: добавьте требуемую валидацию
+
+        user = self.context["request"].user
+
+        # Получаем статус объекта
+        if self.instance:
+            new_status = data.get('status', self.instance.status)
+        else:
+            new_status = data.get('status', 'OPEN')  # статус по умолчанию
+
+        # Проверяем только если OPEN
+        if new_status == 'OPEN':
+            queryset = Advertisement.objects.filter(
+                creator=user,
+                status='OPEN'
+            )
+
+            # При обновлении исключаем текущее объявление
+            if self.instance:
+                queryset = queryset.exclude(pk=self.instance.pk)
+
+            if queryset.count() >= 10:
+                raise serializers.ValidationError(
+                    "У пользователя не может быть больше 10 открытых объявлений"
+                )
 
         return data
